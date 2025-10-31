00bce9fa9df832438dccd4677237ed8ffefa0b97
DjameelEddine <djameleddine.achour@ensia.edu.dz>
2025-10-31 22:09:31 +0100

    feat: Add Djamel's Screens

---
+++ frontend/Books/BookListing/BookListing.js
@@ -0,0 +1,14 @@
+document.querySelector('.filter-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('.filter-popup').classList.toggle('show');
+});
+
+document.querySelectorAll('.book-category').forEach(category => {
+    category.addEventListener('click', () => {
+        category.classList.toggle('active');
+    });
+});
+
+function toggleMenu() {
+    document.getElementById("mobileMenu").classList.toggle("show");
+  }
